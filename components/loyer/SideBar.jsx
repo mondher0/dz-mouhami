54be0927b4289a3ba436bb7ce6e5fd14492bcef3
mondher <110062316+mondher0@users.noreply.github.com>
2024-01-24 13:58:02 +0100

    update

--- components/loyer/SideBar.jsx
+++ components/loyer/SideBar.jsx
@@ -4,12 +4,31 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { IoIosCalendar } from "react-icons/io";
 import { IoIosLogOut } from "react-icons/io";
+import { IoIosNotifications } from "react-icons/io";
+import { CiStar } from "react-icons/ci";
+import { AiOutlineUser } from "react-icons/ai";
 
 const SideBar = () => {
   const path = usePathname();
   const parts = path.split("/");
   const id = parts[2];
-  const links = ["Notifications", "Reviews", "Profil"];
+  const links = [
+    {
+      name: "Notifications",
+      icon: <IoIosNotifications size={30} />,
+      link: "notifications",
+    },
+    {
+      name: "Reviews",
+      icon: <CiStar size={30} />,
+      link: "reviews",
+    },
+    {
+      name: "Profile",
+      icon: <AiOutlineUser size={30} />,
+      link: "profile",
+    },
+  ];
   return (
     <aside className="sticky w-[270px] h-screen flex flex-col items-start p-10">
       <Image src="/assets/logo.svg" width={200} height={200} />
@@ -26,21 +45,21 @@ const SideBar = () => {
         </Link>
         {links.map((link, index) => {
           return (
-            <Link key={index} href={`/loyer/${id}/${link}`}>
+            <Link key={index} href={`/loyer/${id}/${link.link}`}>
               <li
                 className={` ${
                   path === `/loyer/${id}/${link}` && "bg-[#FFC107]"
                 }   flex items-center  gap-4  p-4 w-[200px] rounded-[20px] text-white `}
               >
-                <IoIosCalendar size={30} />
-                {link}
+                {link.icon}
+                {link.name}
               </li>
             </Link>
           );
         })}
       </ul>
       <button className="flex items-center justify-between gap-4 text-[18px] p-4 w-[200px] rounded-[30px] text-black bg-[#FFC107] mt-[150px]">
-        Logout 
+        Logout
         <IoIosLogOut />
       </button>
     </aside>
